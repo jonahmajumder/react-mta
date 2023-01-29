2eaa72e5c952e36a87918fcb178e0e78af8b4978
jonahmajumder <jmajumder9@gmail.com>
2023-01-28 21:18:08 -0800

    parsing trip id

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 import GtfsRealtimeBindings from 'gtfs-realtime-bindings';
 import { MTA, haversineDistance, feedUrl } from './mta';
-import ProtoBuf from 'protobufjs';
 // import DeckGL from '@deck.gl/react';
 // import { ScatterplotLayer } from '@deck.gl/layers';
 // import { Map, StaticMap } from 'react-map-gl';
+
+// const ProtoBuf = require('protobufjs');
+// const nyctSubway = ProtoBuf.loadSync('./src/data/nyct-subway.proto');
 
 const INITIAL_RADIUS = 300; // meters
 const MIN_RADIUS = 10; // meters
 const MAX_RADIUS = 5000; // meters
 
+const TRIP_ID_RE = /(?<originTime>\d+)_(?<routeId>[0-9A-Z]+)\.\.(?<direction>[N|S])(?<pathId>[0-9A-Z]+)/;
 const NUM_TRAINS = 5; // length of departure tables
 
 // const nyctProtoFile = require('./data/nyct-subway.proto');
@@ -28,6 +31,7 @@ const NearbyStationsPage = ((props) => {
     const stationDictsWithDist = MTA.STATIONS.map(d => (
       {...d, ...{Distance: haversineDistance(props.location, [Number(d['GTFS Longitude']), Number(d['GTFS Latitude'])])}}
     )).sort((a,b) => a.Distance - b.Distance);
+    console.log(stationDictsWithDist);
     var stationDictsInRadius;
     if (stationDictsWithDist.some(d => d.Distance < rad)) {
       stationDictsInRadius = stationDictsWithDist.filter(d => d.Distance < rad);
@@ -120,20 +124,33 @@ const Station = ((props) => {
     return tripUpdate.stopTimeUpdate.some(matchingStopTimeUpdate)
   });
 
+  const parseTripId = ((tripId) => {
+    const reArray = TRIP_ID_RE.exec(tripId);
+    if (reArray) {
+      const newMillisec = parseInt(reArray.groups.originTime) * 600;
+      const originMidnight = new Date(new Date().toLocaleDateString("en-US", {timeZone: "America/New_York"}));
+      const originDate = new Date(originMidnight.valueOf() + newMillisec);
+      const tripDict = {...reArray.groups, ...{originDate: originDate}}
+      return tripDict;
+    } else {
+      console.error(`Failed parsing tripId: ${tripId}`);
+    }
+  });
+
   const tripUpdateToDict = ((tripUpdate) => {
-    console.log(tripUpdate);
+    const tripDict = parseTripId(tripUpdate.trip.tripId);
     const matchingUpdate = tripUpdate.stopTimeUpdate.find(matchingStopTimeUpdate);
     const lastUpdate = tripUpdate.stopTimeUpdate[tripUpdate.stopTimeUpdate.length-1];
     const lastStopId = lastUpdate.stopId.replace(/[N|S]$/,'');
     const lastStopName = MTA.STATIONS.find(d => d['GTFS Stop ID'] === lastStopId)['Stop Name'];
     const departure = matchingUpdate.departure;
-    const departureDatetime = departure ? new Date(departure.time * 1000) : null;
+    const departureDate = departure ? new Date(departure.time * 1000) : null;
     const directionMatch = /[N|S]$/.exec(matchingUpdate.stopId);
     const direction = directionMatch ? directionMatch[0] : null;
     return {
       line: tripUpdate.trip.routeId,
-      departure: departureDatetime,
-      direction: direction,
+      departure: departureDate,
+      direction: tripDict.direction,
       lastStop: lastStopName,
     }
   })
@@ -146,16 +163,16 @@ const Station = ((props) => {
     const southTripDicts = tripDicts.filter(d => d.direction === 'S' && d.departure).sort((a,b) => a.departure - b.departure);
     const northTripDicts = tripDicts.filter(d => d.direction === 'N' && d.departure).sort((a,b) => a.departure - b.departure);
   
-    const minutesUntilDatetime = ((datetime) => {
-      const currentDatetime = new Date();
-      return Math.max((datetime - currentDatetime)/60000, 0).toFixed(0);
+    const minutesUntilDate = ((date) => {
+      const currentDate = new Date();
+      return Math.max((date - currentDate)/60000, 0).toFixed(0);
     });
 
     const departureTable = ((tripDicts, label) => {
       return (
         <table className='departureTable' >
           <thead>
-            <th colspan="3" >{label}</th>
+            <th colSpan="3" >{label}</th>
           </thead>
           <tbody>
           {tripDicts.slice(0, NUM_TRAINS).map((d,i) => <tr key={i}>
@@ -164,7 +181,7 @@ const Station = ((props) => {
               alt={d.line + 'icon'}/>
             </td>
             <td >{d.lastStop}</td>
-            <td style={{textAlign: 'right'}} >({minutesUntilDatetime(d.departure)} min)</td>
+            <td style={{textAlign: 'right'}} >({minutesUntilDate(d.departure)} min)</td>
           </tr>)}
           </tbody>
         </table>
@@ -222,7 +239,7 @@ const App = () => {
   const [location, setLocation] = useState([-73.9664714, 40.8057752]);
   console.log('App rerendered.');
 
-  console.log('Location fetching turned off!');
+  // console.log('Location fetching turned off!');
   // useEffect (() => {
   //   navigator.geolocation.getCurrentPosition((pos) => {
   //     const lon = pos.coords.longitude;
